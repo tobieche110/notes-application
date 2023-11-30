ffea2fdc990ce37941bee99b0b5b90f68dcef002
tobieche110 <tobiasechenique@hotmail.com>
2023-11-30 04:38:16 -0300

    Initial commit

--- INSTALL_COMPONENTS.sh
+++ INSTALL_COMPONENTS.sh
@@ -15,7 +15,28 @@ if ! command -v npm &> /dev/null; then
     exit 1
 fi
 
+echo "REMEMBER TO HAVE MYSQL AND APACHE SERVICES RUNNING ON XAMPP"
+echo "REMEMBER TO HAVE MYSQL AND APACHE SERVICES RUNNING ON XAMPP"
+echo "REMEMBER TO HAVE MYSQL AND APACHE SERVICES RUNNING ON XAMPP"
+
+echo "Configuring database..."
+echo "We will assume username: root, password: "
+echo "Please, edit this file or install the database manually if you have an error."
+
+# MySQL Path
+MYSQL_PATH="opt/lampp/bin"
+export PATH="$MYSQL_PATH:$PATH"
+
+# DB config
+DB_USER="root"
+DB_NAME="note_application"
+SQL_FILE="note_application_db.sql"
+
+# Command for installing the db
+mysql -u"${DB_USER}" -p "${DB_NAME}" < "${SQL_FILE}"
+
 echo "Installing backend dependencies..."
+echo "MAKE SURE TO HAVE THE DATABASE INSTALLED"
 cd backend
 mvn clean install
 
@@ -25,23 +46,23 @@ echo "Installing frontend dependencies..."
 cd frontend
 npm install
 
-echo "Configuring database..."
-echo "We will assume username: root, password: "
-echo "Please, edit this file or install the database manually if you have an error."
-
-# DB confir
-DB_USER="root"
-DB_PASSWORD=""
-DB_NAME="note_application"
-SQL_FILE="note_application_db.sql"
-
-# Command for installing the db
-mysql -u"${DB_USER}" -p"${DB_PASSWORD}" "${DB_NAME}" < "${SQL_FILE}"
+cd ..
 
 echo "Running servers..."
+
 cd backend
-mvn spring-boot:run
+
+if [ ! -f "pom.xml" ]; then
+    echo "Error: pom.xml not found in the specified directory."
+    exit 1
+fi
+
+gnome-terminal -- bash -c "mvn spring-boot:run" &
 
 cd ..
+
 cd frontend
+
 npm start
+
+echo "FINISH"
